346011d1c1ff204e7f42a5d3a6b16be828c55221
Ayo <Ayoroq@gmail.com>
2025-12-17 16:55:57 -0500

    started to load data from the backend to the frontend

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function Products() {
-    return (
-        <div>
-            <h1>Products</h1>
-            <p>Here are the products</p>
-        </div>
-    )
+  const API_URL = import.meta.env.VITE_BACKEND_URL;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await fetch(`${API_URL}/products`);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>Here are the products</p>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
