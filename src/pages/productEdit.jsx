90e20248da325f025be2b96005bfdf04e844cb8c
Ayo <Ayoroq@gmail.com>
2025-12-19 01:52:35 -0500

    Added description to the input form

--- src/pages/productEdit.jsx
+++ src/pages/productEdit.jsx
@@ -9,20 +9,22 @@ export default function ProductEdit() {
   const [formData, setFormData] = useState({
     name: "",
     quantity: 0,
-    price: 0.00,
+    price: 0.0,
+    description: "",
     categories: "",
   });
 
   useEffect(() => {
-  if (product) {
-    setFormData({
-      name: product.name,
-      quantity: product.quantity,
-      price: product.price,
-      categories: product.categories,
-    });
-  }
-}, [product]);
+    if (product) {
+      setFormData({
+        name: product.name,
+        quantity: product.quantity,
+        price: product.price,
+        description: product.description,
+        categories: product.categories,
+      });
+    }
+  }, [product]);
 
   useEffect(() => {
     async function fetchProduct() {
@@ -53,7 +55,32 @@ export default function ProductEdit() {
 
   function submitHandler(e) {
     e.preventDefault();
-    
+    async function updateProduct() {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/products/${id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setProduct(data);
+        alert("Product updated successfully!");
+      } catch (error) {
+        console.log(error);
+        alert("Failed to update product.");
+      }
+    }
+    updateProduct();
   }
 
   return (
@@ -68,21 +95,62 @@ export default function ProductEdit() {
           <form className={styles.editForm} onSubmit={submitHandler}>
             <label>
               Name:
-              <input type="text" required value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })}/>
+              <input
+                type="text"
+                required
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
             </label>
             <label>
               Quantity:
-              <input type="number" required value={formData.quantity} onChange={(e) => setFormData({ ...formData, quantity: e.target.value })} />
+              <input
+                type="number"
+                required
+                value={formData.quantity}
+                onChange={(e) =>
+                  setFormData({ ...formData, quantity: e.target.value })
+                }
+              />
             </label>
             <label>
               Price:
-              <input type="number" required step="0.01" value={formData.price} onChange={(e) => setFormData({ ...formData, price: e.target.value })} />
+              <input
+                type="number"
+                required
+                step="0.01"
+                value={formData.price}
+                onChange={(e) =>
+                  setFormData({ ...formData, price: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              Description:
+              <textarea
+                value={formData.description}
+                required
+                onChange={(e) =>
+                  setFormData({ ...formData, description: e.target.value })
+                }
+              />
             </label>
             <label>
               Categories:
-              <input type="text" required value={formData.categories} onChange={(e) => setFormData({ ...formData, categories: e.target.value })} />
+              <input
+                type="text"
+                required
+                value={formData.categories}
+                onChange={(e) =>
+                  setFormData({ ...formData, categories: e.target.value })
+                }
+              />
             </label>
-            <button className={styles.editButton} type="submit">Save Changes</button>
+            <button className={styles.editButton} type="submit">
+              Save Changes
+            </button>
           </form>
         </div>
       )}
