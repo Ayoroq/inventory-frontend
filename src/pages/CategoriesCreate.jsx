1e24a58acc45e93ef55ea19c1a976c1ae61d84f8
Ayo <Ayoroq@gmail.com>
2025-12-20 15:17:41 -0500

    Implement category creation and editing functionality with API integration

--- src/pages/CategoriesCreate.jsx
+++ src/pages/CategoriesCreate.jsx
@@ -1,15 +1,35 @@
 import CategoryForm from "../components/CategoryForm"
+import { useNavigate } from "react-router";
+
 export default function CategoriesCreate() {
-    const onSubmit = (data) => {
-        console.log(data)
+    const navigate = useNavigate();
+   async function submitHandler(formData) {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/categories/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      navigate("/categories");
+    } catch (error) {
+      console.log(error);
     }
+  }
     return (
         <div>
             <h1>Categories Create</h1>
             <p>Here you can create a new category</p>
             <CategoryForm 
                 initialData={{}}
-                onSubmit={onSubmit}
+                onSubmit={submitHandler}
                 submitButtonText="Create Category"
                 submitAction="create" />
         </div>
--- src/pages/categoryEdit.jsx
+++ src/pages/categoryEdit.jsx
@@ -1,15 +1,69 @@
-import CategoryForm from "../components/CategoryForm"
+import CategoryForm from "../components/CategoryForm";
+import { useParams, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import styles from "../styles.module.css";
 
 export default function CategoryEdit() {
-    return (
-        <div>
-            <h1>Category Edit</h1>
-            <p>Here you can edit a category</p>
-            <CategoryForm 
-                initialData={{}}
-                onSubmit={(data) => console.log(data)}
-                submitButtonText="Update Category" 
-                submitAction="edit"/>
-        </div>
-    )
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const [category, setCategory] = useState(null);
+
+  useEffect(() => {
+    async function fetchCategory() {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/categories/${id}`
+        );
+
+        if (response.status === 404) {
+          return;
+        }
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setCategory(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCategory();
+  }, [id]);
+
+  async function submitHandler(formData) {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/categories/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      navigate("/categories");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Category Edit</h1>
+      <p>Here you can edit a category</p>
+      <CategoryForm
+        initialData={category}
+        onSubmit={submitHandler}
+        submitButtonText="Update Category"
+        submitAction="edit"
+      />
+    </div>
+  );
 }
