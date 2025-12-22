1e6a6796a332fca2b785a403ed20b15bf7358ce5
Ayo <Ayoroq@gmail.com>
2025-12-22 08:17:00 -0500

    added gsap and lenis for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { Outlet } from "react-router";
+import gsap from "gsap";
+import { ReactLenis } from "lenis/react";
+import { useRef, useEffect } from "react";
 import Nav from "./components/Nav";
 import "./App.css";
 
-function App() {
+export default function App() {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
-    <>
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
       <Nav />
       <Outlet />
-    </>
+    </ReactLenis>
   );
 }
-
-export default App;
